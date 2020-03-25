e189a2fa8b47f813c405ac74c10acef83c042cc8
iwickham <isaac_wickham@taulor.edu>
2020-03-25 17:14:03 -0400

    Added math to UV sensor

--- UVSensor.c
+++ UVSensor.c
@@ -1,13 +1,15 @@
 #include "adcDCpropab.h"                            // Include adcDCpropAB
 #include "simpletools.h"
 
+const float CONVERSIONCONST = 0.1;
+		
 void uvInit() {
   adc_init(21, 20, 19, 18); //A/D 3,2,1,0
 }
   
 float uvRead() {
-  int uvInput = input(1);
-  float analog = adc_volts(1);
-  print("uvreading: %.6f\n", analog);
-  return analog;
+ float analog = adc_volts(1);
+ float uvVal = analog/CONVERSIONCONST;
+  print("uvreading: %.6f\n", uvVal);
+  return uvVal;
 }  
